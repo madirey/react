46fa7cf667e4c3cc0c3c5009ee291f82b960b6cc
Matt Caldwell <matt.caldwell@gmail.com>
2012-04-14 12:08:25 -0400

    add PIL and complete rgb_histogram calculation... also, modularize feature extraction so that individual features can be requested

--- react/features.py
+++ react/features.py
@@ -1,26 +1,39 @@
-import numpy as np, urllib2
-from django.conf import settings
-#from scipy import ndimage
+import logging, numpy as np, sys, urllib2
+from django.core.cache import cache
+from PIL import Image
+
+try:
+    from scipy import ndimage
+except:
+    from scipy_local import ndimage
 
 try:
     from cStringIO import StringIO
 except:
     from StringIO import StringIO
 
-# from http://stackoverflow.com/questions/6464353/np-array-from-cstringio-object-and-avoiding-copies
-def chariter(filelike):
-    while True:
-        octet = filelike.read(1)
-        if octet:
-            yield ord(octet)
-        else:
-            return
+logger = logging.getLogger('react')
 
-def extract_features(cache_key, url):
+
+def rgb_histogram(im):
+    r = np.asarray(im.convert('RGB', (1,0,0,0, 1,0,0,0, 1,0,0,0) ))
+    g = np.asarray(im.convert('RGB', (0,1,0,0, 0,1,0,0, 0,1,0,0) ))
+    b = np.asarray(im.convert('RGB', (0,0,1,0, 0,0,1,0, 0,0,1,0) ))
+    hr, hr_bins = np.histogram(r, bins=256, density=True)
+    hg, hg_bins = np.histogram(g, bins=256, density=True)
+    hb, hb_bins = np.histogram(b, bins=256, density=True)
+    h_rgb = np.array([hr, hg, hb]).ravel()
+    return h_rgb
+
+def extract_features(cache_key, url, feature_list=['rgb_histogram']):
     if url:
-        print url
+        logger.debug('extracting features for document at: %s' % url)
         response = urllib2.urlopen(url)
-        bits = np.fromstring(StringIO(response.read()).getvalue(), dtype=np.uint8)
+        im = Image.open(StringIO(response.read()))
+        feature_dict = cache.get(cache_key, { }) 
 
-        if settings.DEBUG:
-            pass
+        for feature in feature_list:
+            feature_dict[feature] = getattr(sys.modules[__name__], feature)(im) 
+            cache.set(cache_key, feature_dict)
+            print feature_dict
+
